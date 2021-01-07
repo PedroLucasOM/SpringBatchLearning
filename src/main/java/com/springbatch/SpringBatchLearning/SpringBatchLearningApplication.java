c189f3d6c9e7ab988a1c5297428e40ea9be096ec
Pedro Lucas <pedro99lucasom@gmail.com>
2021-01-07 01:55:15 -0300

    Commit Initial | #Section1 - Primeira Aula Prática

---
+++ src/main/java/com/springbatch/SpringBatchLearning/SpringBatchLearningApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.SpringBatchLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLearningApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springbatch/SpringBatchLearning/config/BatchConfig.java
@@ -0,0 +1,44 @@
+package com.springbatch.SpringBatchLearning.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job sayHelloWorld() {
+        return jobBuilderFactory.get("helloWorld")
+                .start(stepToSayHelloWorld())
+                .build();
+    }
+
+    public Step stepToSayHelloWorld() {
+        return stepBuilderFactory.get("stepToSayHelloWorld")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Hello, world!");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}

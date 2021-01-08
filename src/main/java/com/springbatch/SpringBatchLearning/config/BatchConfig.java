580ff56c41fddbd7143286c3356a163303b8b707
Pedro Lucas <pedro99lucasom@gmail.com>
2021-01-08 15:45:24 -0300

    Configuração com o Banco de Dados e implementação de passagem e captura de argumentos de execução | #Section3 - Aula 13

--- src/main/java/com/springbatch/SpringBatchLearning/config/BatchConfig.java
+++ src/main/java/com/springbatch/SpringBatchLearning/config/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,20 +28,30 @@ public class BatchConfig {
 
     @Bean
     public Job sayHelloWorld() {
-        return jobBuilderFactory.get("helloWorld")
+        return jobBuilderFactory
+                .get("helloWorld")
                 .start(stepToSayHelloWorld())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     public Step stepToSayHelloWorld() {
-        return stepBuilderFactory.get("stepToSayHelloWorld")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello, world!");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+        return stepBuilderFactory
+                .get("stepToSayHelloWorld")
+                .tasklet(sayHelloWorldTasklet(null))
                 .build();
     }
+
+    @Bean
+    @StepScope
+    public Tasklet sayHelloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Hello, %s!", name));
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
 }

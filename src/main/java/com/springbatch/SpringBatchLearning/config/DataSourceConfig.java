92f159280b4be5f711e85918347dd59e7d8e4747
Pedro Lucas <pedro99lucasom@gmail.com>
2021-01-11 02:54:21 -0300

    Configuração de múltiplos bancos | #Section4 - Prática: Configuração de múltiplos bancos

---
+++ src/main/java/com/springbatch/SpringBatchLearning/config/DataSourceConfig.java
@@ -0,0 +1,26 @@
+package com.springbatch.SpringBatchLearning.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource springDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource appDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}

7b3fa7bf1dfd968013879c17b660ab8161d09044
Pedro Lucas <pedro99lucasom@gmail.com>
2021-01-11 02:46:42 -0300

    Configuração de application.properties externo e criação de util para testes | #Section4 - Prática: Externalização de propriedades

---
+++ src/main/java/com/springbatch/SpringBatchLearning/util/Util.java
@@ -0,0 +1,9 @@
+package com.springbatch.SpringBatchLearning.util;
+
+public class Util {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+
+}

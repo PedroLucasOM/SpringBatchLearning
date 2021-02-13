ff391c6a8b9fbe307d489ad50ba36bda743ee913
Pedro Lucas <pedro99lucasom@gmail.com>
2021-02-13 03:06:02 -0300

    Configuração de escritor para persistir registros de conta no banco de dados | #Section7 - Prática: Escrita em banco de dados - JDBC

--- src/main/java/com/springbatch/SpringBatchLearning/writer/config/BankAccountGenerateWriterConfig.java
+++ src/main/java/com/springbatch/SpringBatchLearning/writer/config/BankAccountGenerateWriterConfig.java
@@ -2,15 +2,40 @@ package com.springbatch.SpringBatchLearning.writer.config;
 
 import com.springbatch.SpringBatchLearning.model.BankAccount;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Configuration
 public class BankAccountGenerateWriterConfig {
 
     @Bean
-    public ItemWriter<BankAccount> bankAccountGenerateWriter() {
-        return items -> items.forEach(System.out::println);
+    public JdbcBatchItemWriter<BankAccount> bankAccountGenerateWriter(
+            @Qualifier("appDataSource") DataSource dataSource
+            ) {
+        return new JdbcBatchItemWriterBuilder<BankAccount>()
+                .dataSource(dataSource)
+                .sql("INSERT INTO bank_account (account_type, max_limit, client_id) VALUES (?, ?, ?)")
+                .itemPreparedStatementSetter(itemPreparedStatementSetter())
+                .build();
+    }
+
+    private ItemPreparedStatementSetter<BankAccount> itemPreparedStatementSetter() {
+        return new ItemPreparedStatementSetter<BankAccount>() {
+            @Override
+            public void setValues(BankAccount bankAccount, PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, bankAccount.getBankAccountType().name());
+                preparedStatement.setDouble(2, bankAccount.getMaxLimit());
+                preparedStatement.setString(3, bankAccount.getClientId());
+            }
+        };
     }
 
 }

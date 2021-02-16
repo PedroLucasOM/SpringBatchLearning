b3a554d7e66cb0e9c6292943a31ca4419cd64404
Pedro Lucas <pedro99lucasom@gmail.com>
2021-02-16 20:43:57 -0300

    Code Refactor

--- src/main/java/com/springbatch/SpringBatchLearning/writer/config/BudgetStatementWriterConfig.java
+++ src/main/java/com/springbatch/SpringBatchLearning/writer/config/BudgetStatementWriterConfig.java
@@ -12,11 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.net.MalformedURLException;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
